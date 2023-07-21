4f3d56890e2c3725a4a6b257e86ccd765d5d418d
codeyouka <plants.coordinators@gmail.com>
2023-07-22 00:07:30 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#echo $LINE | sed -e "s/12345678/${replace}/g"
+
+#echo "${PWD}/${0}"
+
+cd ../ || gem install
+echo "${0}"
+#ruby /home/peinfs/Desktop/research/explore/codereview_m1/application/app/main.rb
