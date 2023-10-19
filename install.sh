a9ffa114e0bc4acf5a2a90065b4d6a24c6aae0b5
codeyouka <plants.coordinators@gmail.com>
2023-10-20 07:09:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/bin/sh
+# Copyright 2023 the Cloverpage authors. All rights reserved. MIT license.
+# TODO(everyone): Keep this script simple and easily auditable.
 
-#echo $LINE | sed -e "s/12345678/${replace}/g"
+set -e
 
-#echo "${PWD}/${0}"
+if ! command -v unzip >/dev/null; then
+	echo "Error: unzip is required to install" 1>&2
+	exit 1
+fi
 
-cd ../ || gem install
-echo "${0}"
-#ruby /home/peinfs/Desktop/research/explore/codereview_m1/application/app/main.rb
+if [ "$OS" = "Windows_NT" ]; then
+	target="x86_64-pc-windows-msvc"
+else
+	case $(uname -sm) in
+	"Darwin x86_64") target="x86_64-apple-darwin" ;;
+	"Darwin arm64") target="aarch64-apple-darwin" ;;
+	"Linux aarch64")
+		echo "Error: Official Cloverpage builds for Linux aarch64 are not available. (see: https://github.com/fonipts/cloverpage/issues/1846 )" 1>&2
+		exit 1
+		;;
+	*) target="x86_64-unknown-linux-gnu" ;;
+	esac
+fi
